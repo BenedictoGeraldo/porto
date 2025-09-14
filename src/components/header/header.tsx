eb8a68b33ce265444abbf4e8a25c18b0b8eb873e
BenedictoGeraldo <geraldodawa@gmail.com>
2025-09-14 10:44:01 +0700

    add header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import styles from './Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        NAMA_PANGGILAN_ANDA
+      </div>
+      <nav className={styles.socialIcons}>
+        <a href="https://www.linkedin.com/in/benedicto-geraldo/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={24} />
+        </a>
+        <a href="https://github.com/BenedictoGeraldo" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={24} />
+        </a>
+        <a href="mailto:geraldodawa@gmail.com">
+          <FaEnvelope size={24} />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
